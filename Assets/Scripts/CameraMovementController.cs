70bf942494ae50062a4632a2a9211c597751514a
Shicheng Chu <cheng61746093@gmail.com>
2019-10-10 17:08:01 -0700

    Initial commit
    Initial commit

---
+++ Assets/Scripts/CameraMovementController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementController : MonoBehaviour {
+
+    public float height;
+    float timeCounter = 0;
+
+    private List<GameObject> players = new List<GameObject>();
+    // Use this for initialization
+    void Start () {
+        players.Add(GameObject.Find("Player1"));
+        players.Add(GameObject.Find("Player2"));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CircularMovement();
+        LookAtMidPoint();
+	}
+
+    public void LookAtMidPoint()
+    {
+        GameObject nextPlayer = players[1];
+        GameObject currentPlayer = players[0];
+        Vector3 currentPos = currentPlayer.transform.position;
+        Vector3 lookAtPosition = nextPlayer.gameObject.transform.position;
+        lookAtPosition = (lookAtPosition + currentPos) / 2;
+        this.transform.LookAt(lookAtPosition, Vector3.up);
+    }
+
+    public void CircularMovement()
+    {
+        GameObject p1 = players[0];
+        GameObject p2 = players[1];
+        Vector3 p1Pos = p1.transform.position;
+        Vector3 p2Pos = p2.gameObject.transform.position;
+        Vector3 center = (p1Pos + p2Pos) / 2;
+        float radius = (p1Pos - p2Pos).magnitude * 1.5f;
+        timeCounter += Time.deltaTime; // multiply all this with some speed variable (* speed);
+        float x = radius * Mathf.Cos(timeCounter);
+        float z = radius * Mathf.Sin(timeCounter);
+        float y = 4;
+        Vector3 relativePos = new Vector3(x, y, z);
+
+        transform.position = relativePos + center;
+    }
+
+    public void CircularSwitch()
+    {
+        GameObject p1 = players[0];
+        GameObject p2 = players[1];
+        Vector3 p1Pos = p1.transform.position;
+        Vector3 p2Pos = p2.gameObject.transform.position;
+        Vector3 center = (p1Pos + p2Pos) / 2;
+        float radius = (p1Pos - p2Pos).magnitude * 1.5f;
+        timeCounter += Time.deltaTime; // multiply all this with some speed variable (* speed);
+        float x = radius * Mathf.Cos(timeCounter);
+        float z = radius * Mathf.Sin(timeCounter);
+        float y = 4;
+        Vector3 relativePos = new Vector3(x, y, z);
+
+        transform.position = relativePos + center;
+    }
+}
---
+++ Assets/Scripts/ObjectMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour {
+    float timeCounter = 0;
+    GameObject anotherPlayer;
+    // Use this for initialization
+    void Start () {
+        string nextName = gameObject.name.Equals("Player1") ? "Player2" : "Player1";
+        anotherPlayer = GameObject.Find(nextName);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 offset = new Vector3();
+        if (this.gameObject.name.Equals("Player1"))
+        {
+            timeCounter -= Time.deltaTime * 3;
+            offset = new Vector3(4f, .5f, 4f);
+        }
+        else
+        {
+            timeCounter += Time.deltaTime * 3; // multiply all this with some speed variable (* speed);
+            offset = new Vector3(-4f, .5f, -4f);
+
+        }
+
+        float x = Mathf.Cos(timeCounter);
+        float z = Mathf.Sin(timeCounter);
+        float y = 0;
+        transform.position = (new Vector3(x, y, z)) * 2 + offset;
+        transform.LookAt(anotherPlayer.transform.position);
+    }
+}
